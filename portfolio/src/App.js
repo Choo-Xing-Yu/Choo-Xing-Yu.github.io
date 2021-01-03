0042228b038cab083291c3e42b632f0227b62818
DESKTOP-A3CCRRH\choox <chooxingyu@hotmail.com>
2021-01-03 13:06:42 +0800

    Install react router
    Add constructor to class App

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      title: "Xing Yu",
+
+      headerLinks: [
+        {title: "Home", path: "/"},
+        {title: "About", path: "/about"},
+        {title: "Contact", path: "/contact"},
+      ],
+
+      home: {
+        title: 'Introduction',
+        subtitle: "Projects",
+        text: "Check out my Projects below"
+      },
+
+      about: {
+        title: "About me"
+      },
+
+      contact: {
+        title: "Contact"
+      },
+      
+    }
+  }
+
   render() {
     return (
-      <div> Hi! </div>
+      <div> Hi! My name is Xy </div>
     );
   }
 }
