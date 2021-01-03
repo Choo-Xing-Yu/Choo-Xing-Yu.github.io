2de085f0dd8c9d95553f636e4c9d5c2293374972
DESKTOP-A3CCRRH\choox <chooxingyu@hotmail.com>
2021-01-03 16:04:49 +0800

    Add Footer

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import NavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import './App.css';
+
+import Footer from "./components/Footer";
 
 class App extends React.Component {
 
@@ -28,13 +34,33 @@ class App extends React.Component {
       contact: {
         title: "Contact"
       },
-      
+
     }
   }
 
   render() {
     return (
-      <div> Hi! My name is Xy </div>
+      <Router>
+        <Container className="p-0" fluid={true}>
+          
+          <NavBar className="border-bottom" bg="transparent" expand="lg">
+            <NavBar.Brand>
+              Choo Xing Yu
+            </NavBar.Brand>
+            <NavBar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <NavBar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </NavBar.Collapse>
+          </NavBar>
+
+          <Footer />
+
+        </Container>
+      </Router>
     );
   }
 }
