0b7dc830bc1e6aea7fdb5f1d475840f11c543b08
DESKTOP-A3CCRRH\choox <chooxingyu@hotmail.com>
2021-01-19 14:44:17 +0800

    Add target blank to AboutMeContent for attendancebot

--- portfolio/src/pages/AboutPage.js
+++ portfolio/src/pages/AboutPage.js
@@ -10,7 +10,7 @@ function AboutPage(props) {
             <AboutMeContent>
                 <p>Hi, my name is Xing Yu. I'm a Year 2 NUS Computer Science student with experience in Django, React, Telegram API, Google Sheets API and Outsystems.</p>
                 <p>I'm constantly trying out new things, from writing an entire mobile application for the Airforce as the Sole Developer within 3 months with no prior experience, to playing in a guitar ensemble without any musical background. I believe in the importance of a can-do attitude.</p>
-                <p>My latest project, <a href="https://docs.google.com/document/d/1EyZuCAC7-hACxoJMQ4gUxkjRNk-ghWW84qXmVOE_6rY/edit?usp=sharing">GENUSAttendance_bot</a>, automates the tedious and error-prone task of taking ~50 member's attendance twice weekly. It also provides important attendance analytics after each practice.</p>
+                <p>My latest project, <a href="https://docs.google.com/document/d/1EyZuCAC7-hACxoJMQ4gUxkjRNk-ghWW84qXmVOE_6rY/edit?usp=sharing" target="_blank" rel="noopener noreferrer">GENUSAttendance_bot</a>, automates the tedious and error-prone task of taking ~50 member's attendance twice weekly. It also provides important attendance analytics after each practice.</p>
             </AboutMeContent>
         </div>
     );
