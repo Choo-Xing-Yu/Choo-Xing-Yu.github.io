5c2e2447da79f08410776b399c2cbe753729a096
DESKTOP-A3CCRRH\choox <chooxingyu@hotmail.com>
2021-05-30 19:45:58 +0800

    add img to accordion

--- src/app/body/project/project-accordion/project-accordion.component.ts
+++ src/app/body/project/project-accordion/project-accordion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { google_links } from '../../../google_links';
+const image_folder_dir = '../../../../assets/images/';
 
 @Component({
   selector: 'app-project-accordion',
@@ -13,26 +14,31 @@ export class ProjectAccordionComponent implements OnInit {
 
   projects: {
     title: string;
-    content: string;
-    link: string;
+    contents: Array<string>;
+    url: string;
+    img_path: string;
   }[] = [
     {
       title: 'Training Mobile Application (RSAF)',
-      content:
-        'Achieved Gold under TEA 2019 by Singapore Productivity Association\n' +
-        'Resulted in $74,802.84 annual savings for RSAF\n' +
-        'Written in Outsystems\n' +
+      contents: [
+        'Achieved Gold under TEA 2019 by Singapore Productivity Association.',
+        'Resulted in $74,802.84 annual savings for RSAF.',
+        'Written in Outsystems.',
         'Sole Developer',
-      link: google_links.training_rsaf_url,
+      ],
+      url: google_links.training_rsaf_url,
+      img_path: image_folder_dir + 'rsaf140.jpeg',
     },
     {
       title: 'NUS Guitar Ensemble Attendance Bot',
-      content:
-        'Automated attendance taking of ~50 members\n' +
-        'Provides analytical report on attendance for Exco\n' +
-        'Written in Java with Telegram and GoogleSheets API\n' +
+      contents: [
+        'Automated attendance taking of ~50 members.',
+        'Provides analytical report on attendance for Exco.',
+        'Written in Java with Telegram and GoogleSheets API.',
         'Sole Developer',
-      link: google_links.attendance_bot_url,
+      ],
+      url: google_links.attendance_bot_url,
+      img_path: image_folder_dir + 'genuslogo.png',
     },
   ];
 }
